c94780abde55a2219b14f446c22f524b5f18c9e7
Rogerio <rogerioalves.ee@gmail.com>
2023-12-28 19:53:29 -0300

    remove hidr/polinjus fontes antiga e nova

--- adequador/gtdp/copia_hidr_polinjus.py
+++ adequador/gtdp/copia_hidr_polinjus.py
@@ -1,47 +1,24 @@
-from os.path import join
+from os.path import join, isfile
+from os import remove
 from shutil import copyfile
 from adequador.utils.configuracoes import Configuracoes
 from adequador.utils.nomes import (
     nome_arquivo_hidr,
     nome_arquivo_polinjus,
-    dados_caso,
 )
 
 
-def __vale_fontes_nova(ano, mes, revisao) -> bool:
-    ANO_FONTES_NOVA = 2022
-    MES_FONTES_NOVA = 9
-    RV_FONTES_NOVA = 1
-    condicoes = [
-        (ano > ANO_FONTES_NOVA),
-        (ano == ANO_FONTES_NOVA) & (mes > MES_FONTES_NOVA),
-        (ano == ANO_FONTES_NOVA)
-        & (mes == MES_FONTES_NOVA) * (revisao >= RV_FONTES_NOVA),
-    ]
-    return any(condicoes)
-
-
 def copia_hidr(diretorio: str):
-    ano, mes, revisao = dados_caso(diretorio)
     arquivo = nome_arquivo_hidr()
     arq_destino_hidr = join(diretorio, arquivo)
-    if __vale_fontes_nova(int(ano), int(mes), int(revisao.split("rv")[1])):
-        copyfile(Configuracoes().arquivo_hidr_fontes_nova, arq_destino_hidr)
-    else:
-        copyfile(Configuracoes().arquivo_hidr_fontes_antiga, arq_destino_hidr)
+    copyfile(Configuracoes().arquivo_hidr, arq_destino_hidr)
 
 
 def copia_polinjus(diretorio: str):
-    ano, mes, revisao = dados_caso(diretorio)
-
     arquivo = nome_arquivo_polinjus()
+    # Se existir polinjus.dat no caso, deleta
+    arq_polinjus_dat = join(diretorio, "polinjus.dat")
+    if isfile(arq_polinjus_dat):
+        remove(arq_polinjus_dat)
     arq_destino_polinjus = join(diretorio, arquivo)
-    if __vale_fontes_nova(int(ano), int(mes), int(revisao.split("rv")[1])):
-        copyfile(
-            Configuracoes().arquivo_polinjus_fontes_nova, arq_destino_polinjus
-        )
-    else:
-        copyfile(
-            Configuracoes().arquivo_polinjus_fontes_antiga,
-            arq_destino_polinjus,
-        )
+    copyfile(Configuracoes().arquivo_polinjus, arq_destino_polinjus)
