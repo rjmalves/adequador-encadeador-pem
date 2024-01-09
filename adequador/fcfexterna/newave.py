9196ff1c6cf7a82a1397d43f500c6fd7bd62b99e
pem-ons <rogerio.alves@ons.org.br>
2024-01-09 21:14:30 +0000

    fix fcf externa

--- adequador/fcfexterna/newave.py
+++ adequador/fcfexterna/newave.py
@@ -4,11 +4,16 @@ from os import listdir
 from os.path import join
 from pathlib import Path
 from zipfile import ZipFile
+from datetime import datetime
+from shutil import move
 from adequador.utils.log import Log
 from adequador.utils.configuracoes import Configuracoes
 
 
 class AdequaFCFExternaNEWAVE:
+
+    MESES_POS = [datetime(9999, m, 1) for m in range(1, 13)]
+
     def __init__(
         self,
         caminho_deck: str,
@@ -16,8 +21,8 @@ class AdequaFCFExternaNEWAVE:
     ) -> None:
         self.__caminho_deck = caminho_deck
         self.__arquivo_caso = arquivo_caso
-        self.__caso = Path(caminho_deck).parts[-2]
-        self.__ano, self.__mes, _ = self.__caso.split("_")
+        self.__diretorio_caso = Path(caminho_deck).parts[-2]
+        self.__ano, self.__mes, _ = self.__diretorio_caso.split("_")
         self.__le_nomes_arquivos()
 
     def __le_nomes_arquivos(self):
@@ -45,29 +50,32 @@ class AdequaFCFExternaNEWAVE:
             join(self.__caminho_deck, self.__arquivos.sistema)
         )
         df_mercado = sistema.mercado_energia
-        anos_mercado = df_mercado["data"].dt.year.unique().tolist()
-        anos_estudo = anos_mercado[: dger.num_anos_estudo]
         sistema.mercado_energia = df_mercado.loc[
-            df_mercado["data"].dt.year.isin(anos_estudo)
+            ~df_mercado["data"].isin(AdequaFCFExternaNEWAVE.MESES_POS)
         ]
         sistema.write(join(self.__caminho_deck, self.__arquivos.sistema))
 
         # c_adic.dat
+        ano_inicio = dger.ano_inicio_estudo
+        num_anos = dger.num_anos_estudo
+        ano_final = ano_inicio + num_anos
+        meses_fora_periodo = [datetime(ano_final, m, 1) for m in range(1, 13)]
         cadic = Cadic.read(join(self.__caminho_deck, self.__arquivos.c_adic))
         df_cargas = cadic.cargas
-        anos_cargas = df_cargas["data"].dt.year.unique().tolist()
-        anos_estudo = anos_cargas[: dger.num_anos_estudo]
         cadic.cargas = df_cargas.loc[
-            df_cargas["data"].dt.year.isin(anos_estudo)
+            ~df_cargas["data"].isin(AdequaFCFExternaNEWAVE.MESES_POS)
         ]
-        cadic.write(join(self.__caminho_deck, self.__arquivos.sistema))
+        cadic.cargas = df_cargas.loc[
+            ~df_cargas["data"].isin(meses_fora_periodo)
+        ]
+        cadic.write(join(self.__caminho_deck, self.__arquivos.c_adic))
 
     def __copia_fcf_externa(self):
         df_mapa_fcf = pd.read_csv(
             Configuracoes().arquivo_mapa_fcf_externa, sep=";"
         )
         caso_com_pos = df_mapa_fcf.loc[
-            df_mapa_fcf["caso_sem_pos"] == self.__caso, "caso_completo"
+            df_mapa_fcf["caso_sem_pos"] == self.__diretorio_caso, "caso_completo"
         ].iloc[0]
         caminho_caso_com_pos = join(
             Configuracoes().diretorio_casos_fcf_externa, caso_com_pos, "newave"
@@ -75,7 +83,7 @@ class AdequaFCFExternaNEWAVE:
         # Extrai os arquivos da FCF externa
         arquivo_zip_cortes = [
             a
-            for a in listdir(self.__caminho_deck)
+            for a in listdir(caminho_caso_com_pos)
             if "cortes_" in a and ".zip" in a
         ][0]
         with ZipFile(join(caminho_caso_com_pos, arquivo_zip_cortes), "r") as z:
@@ -84,7 +92,8 @@ class AdequaFCFExternaNEWAVE:
                 "cortes-060.dat": "cortes-pos.dat",
             }
             for arq_origem, arq_destino in mapa_nomes_cortes.items():
-                z.extract(arq_origem, join(self.__caminho_deck, arq_destino))
+                z.extract(arq_origem, caminho_caso_com_pos)
+                move(join(caminho_caso_com_pos, arq_origem), join(self.__caminho_deck, arq_destino))
 
 
 def adequa_fcfexterna_newave(diretorio: str):
